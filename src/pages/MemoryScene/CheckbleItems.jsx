8f9f93c3566d008a6ad8347133e1f4fd800c1483
jackyYam <ziqi.yan@unholster.com>
2024-04-25 16:46:14 +0200

    put all items

--- src/pages/MemoryScene/CheckbleItems.jsx
+++ src/pages/MemoryScene/CheckbleItems.jsx
@@ -16,9 +16,56 @@ const Items = ({ setShowDialog }) => {
         setShowDialog={setShowDialog}
         // 需要给一个child的位置
         position={Fruit.children[6].position || new THREE.Vector3(0, 0, 0)}
+        // 调整透明盒子位置
+        offsetX={0.7}
+        offsetY={0.2}
+        offsetZ={0.3}
         // 调整透明盒子大小
-        scaleX={1}
-        scaleZ={1}
+        scaleX={2.5}
+        scaleY={0.5}
+        scaleZ={1.4}
+      />
+      <primitive object={Camera} scale={2} />
+      <CheckbleItemWrapper
+        setShowDialog={setShowDialog}
+        // 需要给一个child的位置
+        position={Camera.children[0].position || new THREE.Vector3(0, 0, 0)}
+        // 调整透明盒子位置
+        offsetX={0.7}
+        offsetY={0.2}
+        offsetZ={0.3}
+        // 调整透明盒子大小
+        scaleX={0.1}
+        scaleY={0.1}
+        scaleZ={0.1}
+      />
+      <primitive object={Album} scale={2} />
+      <CheckbleItemWrapper
+        setShowDialog={setShowDialog}
+        // 需要给一个child的位置
+        position={Album.children[0].position || new THREE.Vector3(0, 0, 0)}
+        // 调整透明盒子位置
+        offsetX={0.7}
+        offsetY={0.2}
+        offsetZ={0.3}
+        // 调整透明盒子大小
+        scaleX={0.1}
+        scaleY={0.1}
+        scaleZ={0.1}
+      />
+      <primitive object={Diary} scale={2} />
+      <CheckbleItemWrapper
+        setShowDialog={setShowDialog}
+        // 需要给一个child的位置
+        position={Diary.children[0].position || new THREE.Vector3(0, 0, 0)}
+        // 调整透明盒子位置
+        offsetX={0.7}
+        offsetY={0.2}
+        offsetZ={0.3}
+        // 调整透明盒子大小
+        scaleX={0.1}
+        scaleY={0.1}
+        scaleZ={0.1}
       />
     </>
   );
