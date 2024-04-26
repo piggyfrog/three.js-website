92fd56abdd60160aa00172fb4e14c9dd22fcc26e
李晔 LI YE <93631489+piggyfrog@users.noreply.github.com>
2024-04-26 12:40:25 +0800

    add a mask

--- src/components/GameUI.jsx
+++ src/components/GameUI.jsx
@@ -3,6 +3,7 @@ const GameUI = () => {
     <>
       <img src="/images/title.png" alt="Title" className="title-png" />
       <img src="/images/about.png" alt="About" className="about-png" />
+      <img src="/images/mask.jpg" alt="Mask" className="mask-jpg" />
     </>
   );
 };
