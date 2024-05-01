9f11604e4ed99e8276c2659886a8eddff4f97e64
Ziqi Yan <zyan@uc.cl>
2024-05-01 23:36:42 +0200

    global state and Dialog improve

---
+++ src/hooks/store.js
@@ -0,0 +1,24 @@
+import { t } from "i18next";
+import { create } from "zustand";
+
+export const useDialogStore = create((set) => ({
+  isOpen: false,
+  dialogID: "",
+  setOpen: (value) => {
+    set({ isOpen: false });
+    set({ isOpen: true, dialogID: value });
+  },
+  setClose: () => set({ isOpen: false }),
+}));
+
+export const useGameStateStore = create((set) => ({
+  gameState: [],
+  setGameState: (value) => {
+    set((state) => {
+      if (!state.gameState.includes(value)) {
+        return { gameState: [...state.gameState, value] };
+      }
+      return state;
+    });
+  },
+}));
--- src/i18n.js
+++ src/i18n.js
@@ -1,27 +1,19 @@
 import i18n from "i18next";
+import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(initReactI18next).init({
-  debug: true,
-  fallbackLng: "ch",
-  interpolation: {
-    escapeValue: false, // not needed for react as it escapes by default
-  },
-  // language resources
-  resources: {
-    ch: {
-      translation: {
-        fruit:
-          "我记得我小的时候每天最开心的事情，就是用收音机听评书故事，直到家里后来有了电视机......",
-      },
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(Backend) // used to load data from othe directory
+  .init({
+    fallbackLng: "ch", // default language
+    detection: {
+      checkWhitelist: true,
     },
-    en: {
-      translation: {
-        fruit:
-          "I remember the happiest thing for me when I was young was to listen to storytelling with a radio every day, until we had a TV at home later......",
-      },
+    debug: false,
+    interpolation: {
+      escapeValue: false, // no need for react. it escapes by default
     },
-  },
-});
+  });
 
 export default i18n;
